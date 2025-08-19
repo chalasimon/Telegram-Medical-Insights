957d62a6205afcd9ebb2e18d441f4055bc9c3cb4
chalasimon <chally.r@hotmail.com>
2025-08-19 18:17:29 +0300

    feat: define ops for scraping, loading, dbt, and YOLO enrichment

---
+++ dagster/jobs/pipeline.py
@@ -0,0 +1,20 @@
+from dagster import op, job
+@op
+def scrape_telegram_data():
+    # TODO: Add scraping logic
+    pass
+
+@op
+def load_raw_to_postgres():
+    # TODO: Add loading logic
+    pass
+
+@op
+def run_dbt_transformations():
+    # TODO: Add dbt run logic
+    pass
+
+@op
+def run_yolo_enrichment():
+    # TODO: Add YOLO enrichment logic
+    pass
