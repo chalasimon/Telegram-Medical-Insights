6be4aa42728b66a5af0b690ce87120e12f53f28c
chalasimon <chally.r@hotmail.com>
2025-08-19 15:05:43 +0300

    feat: fct_image_detections done

--- telegram_dbt/models/marts/fct_image_detections.sql
+++ telegram_dbt/models/marts/fct_image_detections.sql
@@ -1,13 +1,29 @@
+with messages as (
+    select
+        message_id,
+        channel_id as channel_username
+       -- channel_title
+    from {{ ref('fct_messages') }}
+),
+detections as (
+    select
+        *
+    from {{ ref('stg_image_detections') }} d
+)
 select
-  d.detection_id,
-  d.message_id,
-  m.channel_username,
-  m.channel_title,
-  d.detected_object_class,
-  d.confidence_score,
-  d.image_path,
-  d.model_name,
-  d.run_ts
-from {{ ref('stg_image_detections') }} d
-left join {{ ref('fct_messages') }} m
-  on m.message_id = d.message_id
+    d.message_id,
+    d.detection_id,
+    m.channel_username,
+    --d.channel_title,
+    d.image_path,
+    d.detected_class,
+    d.confidence,
+    d.bbox_xmin,
+    d.bbox_ymin,
+    d.bbox_xmax,
+    d.bbox_ymax,
+    d.model_name
+from detections d
+left join messages m
+    on d.message_id = m.message_id
+
--- telegram_dbt/models/staging/stg_image_detections.sql
+++ telegram_dbt/models/staging/stg_image_detections.sql
@@ -1,16 +1,14 @@
-with src as (
-    select
-        detection_id,
-        message_id::bigint                                  as message_id,
-        image_path::text                                    as image_path,
-        detected_class::text                                as detected_object_class,
-        confidence::double precision                        as confidence_score,
-        bbox_xmin::double precision                         as bbox_xmin,
-        bbox_ymin::double precision                         as bbox_ymin,
-        bbox_xmax::double precision                         as bbox_xmax,
-        bbox_ymax::double precision                         as bbox_ymax,
-        model_name::text                                    as model_name,
-        run_ts
-    from raw.image_detections
-)
-select * from src
+-- models/staging/stg_image_detections.sql
+select
+    detection_id,
+    message_id,
+    image_path,
+    detected_class,
+    confidence,
+    bbox_xmin,
+    bbox_ymin,
+    bbox_xmax,
+    bbox_ymax,
+    model_name
+from raw.image_detections
+
